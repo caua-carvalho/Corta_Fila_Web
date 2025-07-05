a8684a3fe360671a6cf8da8954339c21ae131d05
caua-carvalho <caua.carma033@gmail.com>
2025-07-05 00:09:15 -0300

    reset de todo projeto

--- src/App.js
+++ src/App.js
@@ -1,62 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import LoginForm from './public/screens/login/LoginForm.jsx';
-import RegisterForm from './public/screens/register/RegisterForm.jsx';
-
-import HomeRedirect from './components/HomeRedirect.jsx';
-import BarberHome from './private/screens/BarberHome.jsx';
-import ClientHome from './private/screens/ClientHome.jsx';
-import PrivateRoute from './components/PrivateRoute.jsx';
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rota pública de login */}
-        <Route path="/login" element={<LoginForm />} />
 
-        {/* Rota pública de registro */}
-        <Route path="/register" element={<RegisterForm />} />
-
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <HomeRedirect />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/barber"
-          element={
-            <PrivateRoute>
-              <BarberHome />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/client"
-          element={
-            <PrivateRoute>
-              <ClientHome />
-            </PrivateRoute>
-          }
-        />
-        
-
-        {/* Rota padrão redireciona para /login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* Rotas privadas
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';      
 import App from './App.js';
-import auth from './contexts/AuthContext.js';
 
 const container = document.getElementById('root');
 const root = createRoot(container);                
 
-root.render(
-  <React.StrictMode>
-    <auth.Provider>
-      <App />
-    </auth.Provider>
-  </React.StrictMode>
-);
+root.render();
