f252c0005ef855065ad347be19564e79b0c025d8
caua-carvalho <caua.carma033@gmail.com>
2025-07-08 17:42:33 -0300

    comunicacao com a api de cadastro de barbeiro

--- src/barber/utils/validarInfos.js
+++ src/barber/utils/validarInfos.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.API_BASE_URL;
 
-export async function registerBarberShop(BS_name, BS_bio, BS_photo, email) {
+export async function registerBarberShop(name, bio, photo, email) {
   const response = await axios.post(`${API_BASE_URL}/barbers/register.php`, {
-    BS_name,
-    BS_bio,
-    BS_photo,
+    name,
+    bio,
+    photo,
     email,
     user_id: JSON.parse(localStorage.getItem('authUser')).user_id,
   });
 
   const user = response.data.user ?? response.data;
-  localStorage.setItem('authUser', JSON.stringify(user));
+  localStorage.setItem('authBarber', JSON.stringify(user));
   return user;
 }
