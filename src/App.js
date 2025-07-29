cdb08aa1ab0e761de1a82509e6127c1ab47b81a9
caua-carvalho <caua.carma033@gmail.com>
2025-07-29 16:35:39 -0300

    reset do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-// Gerado pelo Copilot
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './public/screens/register.jsx';
 import Login from './public/screens/login.jsx';
-import BarberDashboard from './barber/screens/BarberDashboard.jsx';
-import BarberRoute from './barber/components/BarberRoute.jsx';
-import BarberRegister from './barber/screens/InfosBarber.jsx';
 
 // Função principal que define as rotas da aplicação
 function App() {
@@ -15,23 +11,6 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        {/* Rotas protegidas do barbeiro */}
-        <Route
-          path="/barber"
-          element={(
-            <BarberRoute>
-              <BarberDashboard />
-            </BarberRoute>
-          )}
-        />
-        <Route
-          path="/barber/infos"
-          element={(
-            <BarberRoute>
-              <BarberRegister />
-            </BarberRoute>
-          )}
-        />
       </Routes>
     </BrowserRouter>
   );
